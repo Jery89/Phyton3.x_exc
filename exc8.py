72c0329db586be1b2d3505f37aa8e565cf54587d
Jerzy <jerzypilarczyk3089@gmail.com>
2018-10-26 20:54:09 +0200

    exc8 some more code

--- exc8.py
+++ exc8.py
@@ -1,9 +1,30 @@
-while True:
+condition = True
+while condition:
 
     print('paper - I, scissors - X, rock - O')
-    player1 = input("first player desision: ")
-    player2 = input("first player desision: ")
+    player1 = raw_input("first player desision: ")
+    player2 = raw_input("first player desision: ")
 
     if player1 == player2:
         print('one more time')
-    if player1 == 'I' and
+    if player1 == 'I':
+        if player2 == 'X':
+            print("Player 2 won!")
+        if player2 == 'O':
+            print("player 1 won!")
+    if player1 == 'X':
+        if player2 == 'O':
+            print("Player 2 won!")
+        if player2 == 'I':
+            print("player 1 won!")
+    if player1 == 'O':
+        if player2 == 'I':
+            print("Player 2 won!")
+        if player2 == 'X':
+            print("player 1 won!")
+
+    decision = raw_input('Again? Yes/No: ')
+    if decision == 'Yes':
+        condition = True
+    else:
+        condition = False
