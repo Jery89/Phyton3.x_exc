c51b8d961fc6518b13ce8c75f4b77169949733d5
Jerzy <jerzypilarczyk3089@gmail.com>
2018-10-26 20:06:06 +0200

    more code

---
+++ exc8.py
@@ -0,0 +1,9 @@
+while True:
+
+    print('paper - I, scissors - X, rock - O')
+    player1 = input("first player desision: ")
+    player2 = input("first player desision: ")
+
+    if player1 == player2:
+        print('one more time')
+    if player1 == 'I' and
