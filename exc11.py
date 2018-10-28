2046cc2dbdf53a1f7a9e31aa6e383753a869e41e
Jerzy <jerzypilarczyk3089@gmail.com>
2018-10-28 15:42:22 +0100

    exc10 some new code and test

---
+++ exc11.py
@@ -0,0 +1,10 @@
+while True:
+    bufor = int(raw_input('Please insert some numebr: '))
+    l_pierwsza = [1, bufor]
+    podzielniki = [x for x in range(1, bufor+1) if bufor % x == 0]
+    if podzielniki == l_pierwsza:
+        print('liczba %i jest liczba pierwsza' % bufor)
+        print('podzielniki:')+str(podzielniki)
+    else:
+        print('liczba %i nie jest liczba pierwsza' % bufor)
+        print('podzielniki:') + str(podzielniki)
